d6641f4b5c8bc6d4a2b374582be8315e7bebc5e8
Dewik6 <101983478+Dewik6@users.noreply.github.com>
2022-03-30 18:57:36 +0100

    Table v2
    Added method to input ready made table objects from a text file into an ArrayList

--- Table.java
+++ Table.java
@@ -1,12 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+/*--------------INFO--------------------
+		Author: D Kalsi (909298)
+----------------------------------------*/
 
 public class Table {
 	
+	private static ArrayList<Table> tables = new ArrayList<Table>();
 	private int tableNum;
 	private int numSeats;
 	private boolean available;
 	private ArrayList<Booking> booking;
+	private static File allTables = new File("tableList.txt");
 	
+		
 	public Table (int num, int seats) {
 		tableNum = num;
 		numSeats = seats;
@@ -15,12 +25,19 @@ public class Table {
 	}
 	
 	public String toString() {
-		return "Table number: " + tableNum + "\nSeats: " + numSeats + "\nAvailable: " + available;
+		return "\nTable: " + tableNum + "\nSeats: " + numSeats + "\nAvailable: " + available;
 	}
-	
+		
 	//Returns the availability of the table (whether or not it has been assigned a booking)
 	public boolean getAvailability() {
 		return available;
+	}
+	
+	//Prints all tables
+	public static void printTables() {
+		for (Table table: tables) {
+			System.out.print(table + "\n");						
+		}
 	}
 	
 	//Prints the booking assigned to the table
@@ -46,6 +63,23 @@ public class Table {
 			available = true;
 		}
 		System.out.println("This table is not booked");
+	}
+	
+	// Reads and inputs table objects from text file to table ArrayList
+	public static void readFile() throws FileNotFoundException{
+		Scanner scan = new Scanner(allTables);
+		
+		while (scan.hasNextLine()) {
+			
+			try {
+				String[] info = scan.nextLine().split(", ");
+				tables.add(new Table(Integer.parseInt(info[0]), Integer.parseInt(info[1])));
+			} 
+			catch (Exception e) {
+				System.out.println("Error bruv...");
+			}
+			
+		}
 	}
 
 }
